474beaa3d04e65b3525943ce52a7972d07f4d8ee
Dylan Callaghan <21831599@sun.ac.za>
2023-09-18 12:32:11 +0200

    Added script to print buggy versions found.
    [Details]
    Added the make_tree.sh script, which creates a tree-like printout of
    which bugs were successfully found in which versions after the
    transplant.
    
    [Note]
    Also updated .gitignore.
    
    [File's changed]
    	modified:   .gitignore
    	new file:   make_tree.sh

---
+++ make_tree.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+  echo "USAGE: ./make_tree.sh <project> [<log dir>]"
+  exit 0
+fi
+project="$1"
+log_dir="versions"
+if [ $# -gt 1 ]; then
+  log_dir="$2"
+fi
+for version in "$log_dir"/"$project"/*; do
+  bugs=""
+  if [ -f "$version/bugs.txt" ]; then
+    bugs="$(cat "$version/bugs.txt" | cut -d ',' -f 1 | uniq | tr '\n' ' ')"
+  fi
+  echo "$bugs"
+done | sort -n -k 1 | while read line; do echo $line | sed 's/ /\n/g' | sort -n | xargs; done
